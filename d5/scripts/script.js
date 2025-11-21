48b61d6f2706adf73046d39305bf63cbac9031b1
Eduardo Henrique Padilha <mizoguchi_eduh@outlook.com>
2025-11-20 23:30:16 -0300

    quase lá

--- d5/scripts/script.js
+++ d5/scripts/script.js
@@ -23,17 +23,17 @@ botao.addEventListener('click', () => {
 });
 
 // Scroll suave para links de navegação
-const navLinks = document.querySelectorAll('#menu ul a.link');
-navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
+const navLinks = document.querySelectorAll('#menu ul a.link'); //vai pegar todas as ancoras com a classe link (a.link)
+navLinks.forEach(link => { //forEach = para cada uma delas 
+  link.addEventListener('click', function(e) { //vao execultar a arrow function - aguardar o 'click' - quando clicar em cima
+    e.preventDefault(); //ele vai evitar o comportamento padrão - no caso, ao clicar, vai no link errado, pois o "header fixo" fica em cima do texto titulo
     const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      const headerHeight = document.querySelector('header').offsetHeight;
-      const targetPosition = target.offsetTop - headerHeight - 20;
-      window.scrollTo({
-        top: targetPosition,
-        behavior: 'smooth'
+    if (target) { //= se tiver um 'href'
+      const headerHeight = document.querySelector('header').offsetHeight; // vai pegar a altura do 'header'
+      const targetPosition = target.offsetTop - headerHeight - 20; // vai mover para o tamanho normal (vai considerar que existe o tamanho normal da caixa do header) menos [-] 20
+      window.scrollTo({ // a "rolagem tipo suave"
+        top: targetPosition, // para a posição do target que ele calculou
+        behavior: 'smooth' // comportamento "suave"
       });
     }
   });
